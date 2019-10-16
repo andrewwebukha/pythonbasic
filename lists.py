1ef1e60728142951adb29e1544d354e8ca757372
andrewwebukha <56317780+andrewwebukha@users.noreply.github.com>
2019-10-16 19:29:09 +0300

    Add files via upload

---
+++ lists.py
@@ -0,0 +1,26 @@
+#lists-is a collection of more than one variables
+#python list
+
+#personal = ["Andrew Webukha", 20, "Nairobi", 1000000.00, 40j]
+#print(len(personal))
+#print(type(personal))
+
+students = ["Stephen", "Kassim" ,"Onyango"]
+
+new_student =["Kalwe"]
+print(students[0])
+all_students= students+new_student
+print(all_students)
+#all_students.append("Patricia")
+print(all_students)
+
+#all_students.clear()
+#print(all_students)
+a=all_students.count("Kalwe")
+b=all_students.index("Kalwe")
+print(a)
+print(b)
+print(all_students)
+print(all_students[1:3])
+
+
